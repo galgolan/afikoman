68113edb2b66ad8519778844651fccf3b3e50f03
ronnyle <ronnyle@gmail.com>
2015-02-08 18:49:02 +0200

    imagemin to also optimize png

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: 'src/main/webapp/assets/images',
-                src: '**/*.{jpg,jpeg}', // we don't optimize PNG files as it doesn't work on Linux. If you are not on Linux, feel free to use '**/*.{png,jpg,jpeg}'
+                src: '**/*.{png,jpg,jpeg}', // we don't optimize PNG files as it doesn't work on Linux. If you are not on Linux, feel free to use '**/*.{png,jpg,jpeg}'
                     dest: '<%= yeoman.dist %>/assets/images'
                 }]
             }
